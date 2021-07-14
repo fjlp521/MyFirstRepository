1a1ca8a5d50666bf7768e32f8b5f41f7ad011a6e
fjlp521 <50369004+fjlp521@users.noreply.github.com>
2021-07-14 17:29:18 +0800

    Update Status.java

--- DeveloperManage/src/service/Status.java
+++ DeveloperManage/src/service/Status.java
@@ -1,20 +1,5 @@
 package service;
 
 public class Status {
-    private final String NAME;
-
-    public Status(String name) {
-        NAME = name;
-    }
-    public static final Status FREE = new Status("FREE");
-    public static final Status VOCATION = new Status("VOCATION");
-    public static final Status BUSY = new Status("BUSY");
-    public String getNAME(){
-        return NAME;
-    }
-
-    @Override
-    public String toString() {
-        return NAME;
-    }
+    FREE,VOCATION,BUSY;
 }
